3d4e54026c032295211f7e1a538ccf74fda258a5
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-04-06 02:01:44 +0530

    Changed Confetti design

--- assembly-game/src/App.jsx
+++ assembly-game/src/App.jsx
@@ -127,7 +127,8 @@ function App() {
 
   return (
    <main>
-       {isGameWon && <Confetti />}
+       {isGameWon && <Confetti recycle={false}
+                               numberOfPieces={1000}/>}
        <header className="app-header">
            <h1>Assembly: EndGame</h1>
            <p>Guess the word  under 8 attempts to keep the programming world safe from assembly!</p>
